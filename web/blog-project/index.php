bc200d7e3394333a417cec1169244364dc5ae378
Behling-Mathew <31866125+Behling-Mathew@users.noreply.github.com>
2019-06-02 00:36:52 -0700

    ...

--- web/blog-project/index.php
+++ web/blog-project/index.php
@@ -43,7 +43,7 @@ $action = filter_input(INPUT_POST, 'action');
 
 // A valid user exists, log them in
     $_SESSION['loggedin'] = TRUE;
-    $_SESSION['message'] = '<p>You have successfully logged in.</p>';
+    $_SESSION['message'] = '<p>You have successfully logged in using ' . $user_email . '.</p>';
     $userData = getUser($user_email);
     $_SESSION['userData'] = $userData;
     header('Location: view/home.php');
