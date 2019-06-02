53cc9e08e36fd580c2fcc67426a71241b4f0aa5c
Behling-Mathew <31866125+Behling-Mathew@users.noreply.github.com>
2019-06-01 19:59:27 -0700

    test 3

--- web/blog-project/index.php
+++ web/blog-project/index.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-require_once '../library/connections.php';
-require_once '../model/categories-model.php';
-require_once '../library/functions.php';
+require_once 'library/connections.php';
+require_once 'model/categories-model.php';
+require_once 'library/functions.php';
 
 $categories = getCategories();
 
