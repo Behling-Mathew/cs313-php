57f49f8d7639058d6abd925647e7e5c8d633fcd4
Behling-Mathew <31866125+Behling-Mathew@users.noreply.github.com>
2019-06-08 01:32:08 -0700

    ...

--- web/blog-project/common/header.php
+++ web/blog-project/common/header.php
@@ -1,9 +1,9 @@
 <?php
       if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === TRUE) {
-        echo '<a id="logout" href="../index.php?action=logout" title="logout">Logout</a>';
-        echo '<a id="account" href="../index.php?action=viewComments" title="My Account">My Account</a>';
+        echo '<a id="logout" href="/blog-project/index.php?action=logout" title="logout">Logout</a>';
+        echo '<a id="account" href="/blog-project/index.php?action=viewComments" title="My Account">My Account</a>';
       } else {
-        echo '<a id="login" href="../index.php?action=login" title="Account Menu">Login</a>';
+        echo '<a id="login" href="/blog-project/index.php?action=login" title="Account Menu">Login</a>';
       }
       if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === TRUE && $_SESSION['userData']['user_first_name']) {
         echo "<p id='welcome'>Welcome, " . $_SESSION['userData']['user_first_name'] . "!</p>";
